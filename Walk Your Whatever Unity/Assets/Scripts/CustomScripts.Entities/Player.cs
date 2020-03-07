ffa3eb5ae4448b977761f6182d4ed6f98500e7c6
James Cho <chojames500@gmail.com>
2020-03-07 02:49:43 -0600

    Outlines for core classes have been added
    Dog.cs, Player.cs, Whatever.cs

---
+++ Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Entities/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomScripts.Entities
+{
+    public class Player : MonoBehaviour
+    {
+        #region Singleton
+        public static Player Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Entities/Whatever.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomScripts.Entities
+{
+    public class Whatever : MonoBehaviour
+    {
+        #region Singleton
+        public static Whatever Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+        #endregion
+
+
+    }
+}
