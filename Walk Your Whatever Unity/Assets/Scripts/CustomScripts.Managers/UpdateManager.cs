40a18dac4c2a415f71b38283530cdf79ae0ad24f
James Cho <chojames500@gmail.com>
2020-03-07 02:49:53 -0600

    Update Manager added

---
+++ Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Managers/UpdateManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomScripts.Managers
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        #region Singleton
+        public static UpdateManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+        #endregion
+
+        public event Action GlobalUpdate;
+        public event Action GlobalFixedUpdate;
+
+        private void Update()
+        {
+            this.GlobalUpdate?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            this.GlobalFixedUpdate.Invoke();
+        }
+    }
+}
