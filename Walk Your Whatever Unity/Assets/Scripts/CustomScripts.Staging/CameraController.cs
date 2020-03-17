ff9ec4d8b0f1560a5b2a46ccae115670a6da5bbb
James Cho <chojames500@gmail.com>
2020-03-17 07:27:49 -0500

    Camera movement added
    Also, it turns out that setting Sensitivity value of 0.02 instead of 0.01 for "Horizontal" axis in the Input Manager makes the controlling of player and dog's movements much smoother

---
+++ Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Staging/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using CustomScripts.Fundamentals;
+using CustomScripts.Entities;
+using CustomScripts.Managers;
+
+namespace CustomScripts.Staging
+{
+    public class CameraController : MonoBehaviour
+    {
+        private void Start()
+        {
+            UpdateManager.Instance.GlobalLateUpdate += this.TrackDownWhatever;
+        }
+
+
+        [SerializeField] private float verticalOffset = 10f;
+        private void TrackDownWhatever()
+        {
+            var whatever = Whatever.Instance.transform;
+
+            var cameraY = whatever.position.y + this.verticalOffset;
+            var cameraZ = whatever.position.z;
+            var newPos = transform.position.Set(y: cameraY, z: cameraZ);
+
+            transform.position = newPos;
+        }
+    }
+}
