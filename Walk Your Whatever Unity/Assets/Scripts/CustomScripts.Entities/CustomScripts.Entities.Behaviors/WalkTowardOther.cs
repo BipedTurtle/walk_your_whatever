c51c74ec9f7d0e01ab5174daf7e3244f150aacb7
James Cho <chojames500@gmail.com>
2020-04-08 04:05:00 -0500

    Walking toward other dogs movement behavior has been added

--- Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Entities/CustomScripts.Entities.Behaviors/WalkTowardOther.cs
+++ Walk Your Whatever Unity/Assets/Scripts/CustomScripts.Entities/CustomScripts.Entities.Behaviors/WalkTowardOther.cs
@@ -21,17 +21,56 @@ namespace CustomScripts.Entities.Behaviors
         private Transform target;
         public (IMovementBehavior behavior, Vector3 movementAdded) GetBehaviorAndMovement()
         {
-            this.target = this.SetTarget();
-            var towardTargetMovement = (this.target.position - Whatever.Instance.Position) * Time.fixedDeltaTime;
+            if (this.target == null)
+                this.target = this.SetTarget();
 
-            return (this, towardTargetMovement);
+            var movement = this.GetMovementToTarget();
+            this.LoseTargetIfOutOfSight();
+            return (this, movement);
         }
 
-        private Transform SetTarget() =>
-            this.target =
-                this.otherDogs
-                    .Select(d => new { Dog = d, Distance = (d.transform.position - Whatever.Instance.Position).sqrMagnitude })
-                    .Aggregate(func: (current, next) => current.Distance > next.Distance ? next : current)
-                    .Dog.transform;
+
+        private Transform SetTarget()
+        {
+            var thereAreNoOtherDogs = this.otherDogs.Count == 0;
+            if (thereAreNoOtherDogs)
+                return null;
+
+            return 
+                this.target =
+                    this.otherDogs
+                        .Select(d => new { Dog = d, Distance = (d.transform.position - Whatever.Instance.Position).sqrMagnitude })
+                        .Aggregate(func: (current, next) => current.Distance > next.Distance ? next : current)
+                        .Dog.transform;
+        }
+
+
+        private float targetDistanceThreshold = Mathf.Sqrt(3 * 3 + 3 * 3);
+        private Vector3 ToTarget => this.target == null ? Vector3.zero : this.target.position - Whatever.Instance.Position;
+        private bool TargetWithinRange => ToTarget.magnitude < targetDistanceThreshold;
+        private void LoseTargetIfOutOfSight()
+        {
+            bool targetOutOfRange = !this.TargetWithinRange;
+            if (targetOutOfRange)
+                this.target = null;
+        }
+
+
+        // this might not work in a curved map since we're using position.z here
+        private Vector3 GetMovementToTarget()
+        {
+            if (this.target == null)
+                return Vector3.zero;
+
+            var hasPassedOther = Whatever.Instance.Position.z > this.target.position.z;
+            float speed = hasPassedOther ? .9f : 1.3f;
+            Vector3 horizontalBias = Vector3.right * this.ToTarget.x * .48f;
+            Vector3 verticalBias = Vector3.back * this.ToTarget.z * .3f;
+            Vector3 movement = this.ToTarget.normalized + horizontalBias + verticalBias;
+            return
+                this.TargetWithinRange ?
+                    movement * speed * Time.fixedDeltaTime :
+                    Vector3.zero;
+        }
     }
 }
